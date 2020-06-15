307b8dfaf0ef1cb1e1721d12bb7eadd7767da4b9
allen Hsu <nzi90505981@gmail.com>
2020-06-15 11:25:05 +0800

    add chapter 2_1

---
+++ python/ch1/sourcecode/2_1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 19:25:07 2019
+
+@author: Jerry
+"""
+
+a=11
+b=4
+print(a+b)			
+print(a-b)			
+print(a*b)			
+print(a/b)      	
+print(a//b)		
+print(a%b)			
+print(a**b)		
+
+
+
+
+    
